a8f9e7756933273d46b8bfe19a6cc0072b132a7f
yashsahane-eng <sahaneyash2@GMAIL.COM>
2026-07-29 12:43:33 +0530

    Fix MongoDB environment variable name

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect(process.env.MONGO_URI as string);
 
     console.log("MongoDB Connected");
   } catch (error) {
